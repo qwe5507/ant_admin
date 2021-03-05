bb608f25b4d9c64fb731e5d6c2c758c5aa3872f1
mandoo <nattty@gmail.com>
2021-03-05 17:02:40 +0900

    2021-03-05 지표 크롤링 목록

--- src/ants/crawling.react.js
+++ src/ants/crawling.react.js
@@ -9,11 +9,8 @@ import "datatables.net-dt/js/dataTables.dataTables"
 import "datatables.net-dt/css/jquery.dataTables.min.css"
 import $ from 'jquery'; 
 // import { Alert } from 'react-bootstrap';
-import BoardApiService from "../api/BoardApi";
-import CommentApiService from "../api/CommentApi";
+import IndApiService from "../api/IndApi";
 import axios from 'axios';
-import './datatables.css';
-
 
 import {
   Container,
@@ -35,53 +32,408 @@ import {
 import SiteWrapper from "../SiteWrapper.react";
 
 function CrawlingPage() {
+  let [usdkrw, usdkrwbyun] = useState([]);
+  let [eurusd, eurusdbyun] = useState([]);
+  let [usdgbp, usdgbpbyun] = useState([]);
+  let [usdjpy, usdjpybyun] = useState([]);
+  let [usdcny, usdcnybyun] = useState([]);
+  
+  let [wti,wtibyun] = useState([]);
+  let [dolleridx,dolleridxbyun] = useState([]);
+  let [bond10,bond10byun] = useState([]);
+  let [bond2,bond2byun] = useState([]);
+  let [goldfor,goldforbyun] = useState([]);
+  let [bitcoin,bitcoinbyun] = useState([]);
+  
+  let indicator = {
+    'name' : "",
+  }
+    useEffect(() => {
+      console.log("유즈이펙트 실행")
+      bitcoinGet();
+      dolleridxGet();
+      bond2Get();
+      bond10Get();
+      goldforGet();
+      wtiGet();
+      usdkrwGet();
+      eurusdGet();
+      usdcnyGet();
+      usdgbpGet();
+      usdjpyGet();
+    }, []);
+
+    function usdkrwGet(){
+      IndApiService.chartIndi(1)
+      .then(res => {
+        console.log(res.data)
+        usdkrwbyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function eurusdGet(){
+      IndApiService.chartIndiExeFor("EURUSD", 1)
+      .then(res => {
+        console.log(res.data)
+        eurusdbyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function usdcnyGet(){
+      IndApiService.chartIndiExeFor("USDCNY", 1)
+      .then(res => {
+        console.log(res.data)
+        usdcnybyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function usdgbpGet(){
+      IndApiService.chartIndiExeFor("USDGBP", 1)
+      .then(res => {
+        console.log(res.data)
+        usdgbpbyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function usdjpyGet(){
+      IndApiService.chartIndiExeFor("USDJPY", 1)
+      .then(res => {
+        console.log(res.data)
+        usdjpybyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function wtiGet(){
+      IndApiService.indicators1("wti", 1)
+      .then(res => {
+        console.log(res.data)
+        wtibyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+    function dolleridxGet(){
+      IndApiService.indicators2("dolleridx", 1)
+      .then(res => {
+        console.log(res.data)
+        dolleridxbyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                 destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+  function bond2Get(){
+      IndApiService.indicators2("bond2", 1)
+      .then(res => {
+        console.log(res.data)
+        bond2byun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+  function bond10Get(){
+      IndApiService.indicators2("bond10", 1)
+      .then(res => {
+        console.log(res.data)
+        bond10byun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+  
+    function goldforGet(){
+      IndApiService.indicators1("goldfor", 1)
+      .then(res => {
+        console.log(res.data)
+        goldforbyun(res.data)    
+         
+        setTimeout(()=>{                        
+          $('#example').DataTable(
+              {
+                "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                destroy: true
+              }
+            );
+          },100)
+        })
+      .catch(err => {
+        console.log(err);
+      }); 
+    }
+
+  function bitcoinGet(){
+    IndApiService.indicators2("bitcoin", 1)
+    .then(res => {
+      console.log(res.data)
+      bitcoinbyun(res.data)    
+       
+      setTimeout(()=>{                        
+        $('#example').DataTable(
+            {
+              "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+              destroy: true
+            }
+          );
+        },100)
+      })
+    .catch(err => {
+      console.log(err);
+    }); 
+  }
 
   return (
 <SiteWrapper>
-<Page.Content title="Crawling List">
-
-            <Grid.Row>
-            <Grid.Col xl={3} lg={3} md={3} sm={3} xs={0}>
-
-            </Grid.Col>
-            
-            <Grid.Col xl={6} lg={6} md={6} sm={6} xs={12}>
-
-          <table id="example" className="table">
+<Page.Content title="Cralling">
+        <Grid.Row>
+          <Grid.Col md={12} xl={12}>
+           
+          <table id="example" className="display">
             <thead>
-                <tr>
-                    <th>게시판ID</th>
-                    <th>홈닉네임</th>
-                    <th>제목</th>
-                    <th>내용</th>
-                    <th>작성일</th>
-                    <th>수정일</th>
-                    <th>조회수</th>
-                    <th>좋아요수</th>
-                    <th>숨김</th>
-                    <th>관리자ID</th>
+                <tr align = "center">
+                    <th >최신일자</th>
+                    <th>지표이름</th>
+                    <th>크롤링</th>
                 </tr>
             </thead>
             <tbody>
+              {
+                  usdkrw.map(function(data){
+                   return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>원/달러</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })   
+              }
+              {
+                
+                  eurusd.map(function(data){
+                   return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>달러/유로</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+              {
+                
+                  usdgbp.map(function(data){
+                   return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>영국 파운드/달러</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+              {
+                
+                  usdjpy.map(function(data){
+                   return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>일본 엔/달러</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+              {
+                 usdcny.map(function(data){
+                  return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>중국 위안/달러</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+                {
+                 dolleridx.map(function(data){
+                  return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>달러인덱스</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+              {
+
+                  wti.map(function(data){
+                   return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>WTI</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                     )
+                    })
+              }
+              {
+                goldfor.map(function(data){
+                  return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>국제 금</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                    )
+                  })
+              }
+              {
+                 bond10.map(function(data){
+                  return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>미 10년 채권수익률</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                    )
+                  })
+              }
+              {
+                bond2.map(function(data){
+                  return(
+                  <tr align = "center">
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>미 2년 채권수익률</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                  </tr>
+                   )
+                  })
+              }
+              {
+                 bitcoin.map(function(data){
+                  return(
+                  <tr align = "center">
+                   
+                      <td>{data['dates'].substring(0,10)}</td>
+                      <td>비트코인</td>
+                      <td><Button color="red">Cralling Start</Button> </td>
+                    
+                  </tr>
+                    )
+                  })
+              }
              
             </tbody>
             <tfoot>
-                <tr>
-                  <th>유저ID</th>
-                    <th>이메일</th>
-                    <th>카카오닉네임</th>
-                    <th>홈닉네임</th>
-                    <th>핸드폰</th>
-                    <th>가입일</th>
-                    <th>구독</th>
-                    <th>차단</th>
-                    <th>수정</th>
-                    <th>삭제</th>
-                </tr>
+                
             </tfoot>
         </table>
+    
           </Grid.Col>
-
+         
         </Grid.Row>
       </Page.Content>
     </SiteWrapper>
